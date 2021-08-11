0691a695c715f1943bcd2b3bb305e1b97fa8ded7
Clinton Pinto <ctpinto27@gmail.com>
2021-08-10 18:21:44 -0700

    Initial Commit

---
+++ OpenGLCherno/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  OpenGLCherno
+//
+//  Created by Clinton Pinto on 8/10/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
